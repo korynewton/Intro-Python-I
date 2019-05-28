8d295ef97d9a8904ac899684288e3fd1d85ac6a0
Kory Newton <korybnewton@gmail.com>
2019-05-28 12:03:52 -0700

    modules

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,23 +9,22 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+for i in range(len(sys.argv)):
+    print(sys.argv[i])
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
-
+print("OS platform:", sys.platform)
 # Print out the version of Python you're using:
-# YOUR CODE HERE
-
+print("python version (major):", sys.version_info.major)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
-
+print("pid:", os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print("current working directory:", os.getcwd())
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print("login name:", os.getlogin())
